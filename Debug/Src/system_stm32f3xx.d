4d11e47e7f3d29fa36f4364e24b3156bc2cfc405
Woraphop Limpisuk <panpoppular@gmail.com>
2018-12-18 13:35:44 +0700

    Initial commit

---
+++ Debug/Src/system_stm32f3xx.d
@@ -0,0 +1,88 @@
+Src/system_stm32f3xx.o: ../Src/system_stm32f3xx.c \
+ E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f3xx.h \
+ E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f303xc.h \
+ E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/CMSIS/Include/core_cm4.h \
+ E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/CMSIS/Include/core_cmInstr.h \
+ E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/CMSIS/Include/cmsis_gcc.h \
+ E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/CMSIS/Include/core_cmFunc.h \
+ E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/CMSIS/Include/core_cmSimd.h \
+ E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/CMSIS/Device/ST/STM32F3xx/Include/system_stm32f3xx.h \
+ E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal.h \
+ E:/Ac6/Prj/FinalProject/FinalTest2/Inc/stm32f3xx_hal_conf.h \
+ E:/Ac6/Prj/FinalProject/FinalTest2/Inc/main.h \
+ E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc.h \
+ E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_def.h \
+ E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc_ex.h \
+ E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio.h \
+ E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio_ex.h \
+ E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma.h \
+ E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma_ex.h \
+ E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_cortex.h \
+ E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash.h \
+ E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash_ex.h \
+ E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c.h \
+ E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c_ex.h \
+ E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr.h \
+ E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr_ex.h \
+ E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_tim.h \
+ E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_tim_ex.h \
+ E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_uart.h \
+ E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_uart_ex.h
+
+E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f3xx.h:
+
+E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f303xc.h:
+
+E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/CMSIS/Include/core_cm4.h:
+
+E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/CMSIS/Include/core_cmInstr.h:
+
+E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/CMSIS/Include/core_cmFunc.h:
+
+E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/CMSIS/Include/core_cmSimd.h:
+
+E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/CMSIS/Device/ST/STM32F3xx/Include/system_stm32f3xx.h:
+
+E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal.h:
+
+E:/Ac6/Prj/FinalProject/FinalTest2/Inc/stm32f3xx_hal_conf.h:
+
+E:/Ac6/Prj/FinalProject/FinalTest2/Inc/main.h:
+
+E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc.h:
+
+E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_def.h:
+
+E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc_ex.h:
+
+E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio.h:
+
+E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio_ex.h:
+
+E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma.h:
+
+E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma_ex.h:
+
+E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_cortex.h:
+
+E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash.h:
+
+E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash_ex.h:
+
+E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c.h:
+
+E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c_ex.h:
+
+E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr.h:
+
+E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr_ex.h:
+
+E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_tim.h:
+
+E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_tim_ex.h:
+
+E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_uart.h:
+
+E:/Ac6/Prj/FinalProject/FinalTest2/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_uart_ex.h:
---
+++ Src/main.c
@@ -0,0 +1,513 @@
+
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * This notice applies to any and all portions of this file
+  * that are not between comment pairs USER CODE BEGIN and
+  * USER CODE END. Other portions of this file, whether 
+  * inserted by the user or by software development tools
+  * are owned by their respective copyright owners.
+  *
+  * Copyright (c) 2561 STMicroelectronics International N.V. 
+  * All rights reserved.
+  *
+  * Redistribution and use in source and binary forms, with or without 
+  * modification, are permitted, provided that the following conditions are met:
+  *
+  * 1. Redistribution of source code must retain the above copyright notice, 
+  *    this list of conditions and the following disclaimer.
+  * 2. Redistributions in binary form must reproduce the above copyright notice,
+  *    this list of conditions and the following disclaimer in the documentation
+  *    and/or other materials provided with the distribution.
+  * 3. Neither the name of STMicroelectronics nor the names of other 
+  *    contributors to this software may be used to endorse or promote products 
+  *    derived from this software without specific written permission.
+  * 4. This software, including modifications and/or derivative works of this 
+  *    software, must execute solely and exclusively on microcontroller or
+  *    microprocessor devices manufactured by or for STMicroelectronics.
+  * 5. Redistribution and use of this software other than as permitted under 
+  *    this license is void and will automatically terminate your rights under 
+  *    this license. 
+  *
+  * THIS SOFTWARE IS PROVIDED BY STMICROELECTRONICS AND CONTRIBUTORS "AS IS" 
+  * AND ANY EXPRESS, IMPLIED OR STATUTORY WARRANTIES, INCLUDING, BUT NOT 
+  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+  * PARTICULAR PURPOSE AND NON-INFRINGEMENT OF THIRD PARTY INTELLECTUAL PROPERTY
+  * RIGHTS ARE DISCLAIMED TO THE FULLEST EXTENT PERMITTED BY LAW. IN NO EVENT 
+  * SHALL STMICROELECTRONICS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+  * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "stm32f3xx_hal.h"
+#include "cmsis_os.h"
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private variables ---------------------------------------------------------*/
+UART_HandleTypeDef huart2;
+DMA_HandleTypeDef hdma_usart2_rx;
+
+osThreadId UartReciverHandle;
+osTimerId RefresherHandle;
+osTimerId BuzzertogglerHandle;
+osTimerId UartSenderHandle;
+osSemaphoreId myBinarySem01Handle;
+
+/* USER CODE BEGIN PV */
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_DMA_Init(void);
+static void MX_USART2_UART_Init(void);
+void StartDefaultTask(void const * argument);
+void RefreshCall(void const * argument);
+void buzToggle(void const * argument);
+void Callback03(void const * argument);
+
+/* USER CODE BEGIN PFP */
+/* Private function prototypes -----------------------------------------------*/
+
+/* USER CODE END PFP */
+
+/* USER CODE BEGIN 0 */
+char inboundRecive[2];
+uint8_t sensorStatus[8];
+uint8_t sensorAlarm[8];
+int sensornum = 7;
+uint8_t tog = 0;
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  *
+  * @retval None
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration----------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_USART2_UART_Init();
+  /* USER CODE BEGIN 2 */
+
+  /* USER CODE END 2 */
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* Create the semaphores(s) */
+  /* definition and creation of myBinarySem01 */
+  osSemaphoreDef(myBinarySem01);
+  myBinarySem01Handle = osSemaphoreCreate(osSemaphore(myBinarySem01), 1);
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* Create the timer(s) */
+  /* definition and creation of Refresher */
+  osTimerDef(Refresher, RefreshCall);
+  RefresherHandle = osTimerCreate(osTimer(Refresher), osTimerPeriodic, NULL);
+
+  /* definition and creation of Buzzertoggler */
+  osTimerDef(Buzzertoggler, buzToggle);
+  BuzzertogglerHandle = osTimerCreate(osTimer(Buzzertoggler), osTimerPeriodic, NULL);
+
+  /* definition and creation of UartSender */
+  osTimerDef(UartSender, Callback03);
+  UartSenderHandle = osTimerCreate(osTimer(UartSender), osTimerPeriodic, NULL);
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  osTimerStart(BuzzertogglerHandle,500);
+  osTimerStart(RefresherHandle,1000);
+  osTimerStart(UartSenderHandle,1000);
+  /* USER CODE END RTOS_TIMERS */
+
+  /* Create the thread(s) */
+  /* definition and creation of UartReciver */
+  osThreadDef(UartReciver, StartDefaultTask, osPriorityNormal, 0, 128);
+  UartReciverHandle = osThreadCreate(osThread(UartReciver), NULL);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  /* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+ 
+
+  /* Start scheduler */
+  osKernelStart();
+  
+  /* We should never get here as control is now taken by the scheduler */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+
+  /* USER CODE END WHILE */
+
+  /* USER CODE BEGIN 3 */
+
+  }
+  /* USER CODE END 3 */
+
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_PeriphCLKInitTypeDef PeriphClkInit;
+
+    /**Initializes the CPU, AHB and APB busses clocks 
+    */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV4;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL12;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+    /**Initializes the CPU, AHB and APB busses clocks 
+    */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART2;
+  PeriphClkInit.Usart2ClockSelection = RCC_USART2CLKSOURCE_PCLK1;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+    /**Configure the Systick interrupt time 
+    */
+  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+
+    /**Configure the Systick 
+    */
+  HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+
+  /* SysTick_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(SysTick_IRQn, 15, 0);
+}
+
+/* USART2 init function */
+static void MX_USART2_UART_Init(void)
+{
+
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 38400;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+}
+
+/** 
+  * Enable DMA controller clock
+  */
+static void MX_DMA_Init(void) 
+{
+  /* DMA controller clock enable */
+  __HAL_RCC_DMA1_CLK_ENABLE();
+
+  /* DMA interrupt init */
+  /* DMA1_Channel6_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA1_Channel6_IRQn, 5, 0);
+  HAL_NVIC_EnableIRQ(DMA1_Channel6_IRQn);
+
+}
+
+/** Configure pins as 
+        * Analog 
+        * Input 
+        * Output
+        * EVENT_OUT
+        * EXTI
+     PA5   ------> SPI1_SCK
+     PA6   ------> SPI1_MISO
+     PA7   ------> SPI1_MOSI
+     PA11   ------> USB_DM
+     PA12   ------> USB_DP
+     PB6   ------> I2C1_SCL
+     PB7   ------> I2C1_SDA
+*/
+static void MX_GPIO_Init(void)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct;
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOE_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOF_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOE, CS_I2C_SPI_Pin|LD4_Pin|LD3_Pin|LD5_Pin 
+                          |LD7_Pin|LD9_Pin|LD10_Pin|LD8_Pin 
+                          |LD6_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : DRDY_Pin MEMS_INT3_Pin MEMS_INT4_Pin MEMS_INT1_Pin 
+                           MEMS_INT2_Pin */
+  GPIO_InitStruct.Pin = DRDY_Pin|MEMS_INT3_Pin|MEMS_INT4_Pin|MEMS_INT1_Pin 
+                          |MEMS_INT2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_EVT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : CS_I2C_SPI_Pin LD4_Pin LD3_Pin LD5_Pin 
+                           LD7_Pin LD9_Pin LD10_Pin LD8_Pin 
+                           LD6_Pin */
+  GPIO_InitStruct.Pin = CS_I2C_SPI_Pin|LD4_Pin|LD3_Pin|LD5_Pin 
+                          |LD7_Pin|LD9_Pin|LD10_Pin|LD8_Pin 
+                          |LD6_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : B1_Pin */
+  GPIO_InitStruct.Pin = B1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : SPI1_SCK_Pin SPI1_MISO_Pin SPI1_MISOA7_Pin */
+  GPIO_InitStruct.Pin = SPI1_SCK_Pin|SPI1_MISO_Pin|SPI1_MISOA7_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.Alternate = GPIO_AF5_SPI1;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : DM_Pin DP_Pin */
+  GPIO_InitStruct.Pin = DM_Pin|DP_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  GPIO_InitStruct.Alternate = GPIO_AF14_USB;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : SENSOR0_Pin SENSOR1_Pin SENSOR2_Pin SENSOR3_Pin 
+                           SENSOR4_Pin SENSOR5_Pin SENSOR6_Pin SENSOR7_Pin */
+  GPIO_InitStruct.Pin = SENSOR0_Pin|SENSOR1_Pin|SENSOR2_Pin|SENSOR3_Pin 
+                          |SENSOR4_Pin|SENSOR5_Pin|SENSOR6_Pin|SENSOR7_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : I2C1_SCL_Pin I2C1_SDA_Pin */
+  GPIO_InitStruct.Pin = I2C1_SCL_Pin|I2C1_SDA_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.Alternate = GPIO_AF4_I2C1;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 4 */
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart){
+	/*OPCODE:
+	 * A: Activate Alarm
+	 * D: Deactivate Alarm
+	 * NUM: Sensor ID
+	 */
+	char strOpcode = inboundRecive[0];
+	char strNum = inboundRecive[1];
+	sensornum = strNum - 48;
+	if (strOpcode == 'A'){
+		sensorAlarm[sensornum] = 1;
+	}
+	else if(strOpcode == 'D'){
+		sensorAlarm[sensornum] = 0;
+	}
+}
+/* USER CODE END 4 */
+
+/* StartDefaultTask function */
+void StartDefaultTask(void const * argument)
+{
+
+  /* USER CODE BEGIN 5 */
+  /* Infinite loop */
+  for(;;)
+  {
+	 HAL_UART_Receive_DMA(&huart2,inboundRecive,2);
+    osDelay(1);
+  }
+  /* USER CODE END 5 */ 
+}
+
+/* RefreshCall function */
+void RefreshCall(void const * argument)
+{
+  /* USER CODE BEGIN RefreshCall */
+	sensorStatus[0] = HAL_GPIO_ReadPin(GPIOD,SENSOR0_Pin);
+	sensorStatus[1] = HAL_GPIO_ReadPin(GPIOD,SENSOR1_Pin);
+	sensorStatus[2] = HAL_GPIO_ReadPin(GPIOD,SENSOR2_Pin);
+	sensorStatus[3] = HAL_GPIO_ReadPin(GPIOD,SENSOR3_Pin);
+	sensorStatus[4] = HAL_GPIO_ReadPin(GPIOD,SENSOR4_Pin);
+	sensorStatus[5] = HAL_GPIO_ReadPin(GPIOD,SENSOR5_Pin);
+	sensorStatus[6] = HAL_GPIO_ReadPin(GPIOD,SENSOR6_Pin);
+	sensorStatus[7] = HAL_GPIO_ReadPin(GPIOD,SENSOR7_Pin);
+	HAL_GPIO_TogglePin(GPIOE,LD10_Pin);
+  
+  /* USER CODE END RefreshCall */
+}
+
+/* buzToggle function */
+void buzToggle(void const * argument)
+{
+  /* USER CODE BEGIN buzToggle */
+	tog = (tog + 1)%2;
+	HAL_GPIO_TogglePin(GPIOE,LD9_Pin);
+	HAL_GPIO_WritePin(GPIOE,LD3_Pin,sensorAlarm[1]&&tog);
+	HAL_GPIO_WritePin(GPIOE,LD4_Pin,sensorAlarm[2]&&tog);
+  /* USER CODE END buzToggle */
+}
+
+/* Callback03 function */
+void Callback03(void const * argument)
+{
+  /* USER CODE BEGIN Callback03 */
+  HAL_UART_Transmit(&huart2,sensorStatus,8,5);
+  /* USER CODE END Callback03 */
+}
+
+/**
+  * @brief  Period elapsed callback in non blocking mode
+  * @note   This function is called  when TIM1 interrupt took place, inside
+  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+  * a global variable "uwTick" used as application time base.
+  * @param  htim : TIM handle
+  * @retval None
+  */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  /* USER CODE BEGIN Callback 0 */
+
+  /* USER CODE END Callback 0 */
+  if (htim->Instance == TIM1) {
+    HAL_IncTick();
+  }
+  /* USER CODE BEGIN Callback 1 */
+
+  /* USER CODE END Callback 1 */
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @param  file: The file name as string.
+  * @param  line: The line in file as a number.
+  * @retval None
+  */
+void _Error_Handler(char *file, int line)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  while(1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{ 
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
